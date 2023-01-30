dfce10a9655f923e024b52a7afdb453b23674ea8
oibene <alvespbene@gmail.com>
2023-01-30 10:14:32 -0300

    tentando decifrar como fazer tudo funcionar

--- MushroomParty-App/src/app/app.module.ts
+++ MushroomParty-App/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    ShowcaseComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
